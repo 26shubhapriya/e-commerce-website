268c7072443c82e6209db773f77d9d6991eb34c6
26shubhapriya <shubhapriya1998@gmail.com>
2026-07-08 19:59:45 +0530

    updated error

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,88 +1,100 @@
-import React,{createContext,useState, useEffect,useContext} from 'react';
-import AuthContext from "./auth-context"
-import axios from 'axios';
+import React, { createContext, useState, useEffect, useContext } from "react";
+import AuthContext from "./auth-context";
+import axios from "axios";
 
-  const MyContext= createContext();
+const MyContext = createContext();
 
 const AppContext = (props) => {
-  const authCtx= useContext(AuthContext)
-    
-  const productList=[
+  const authCtx = useContext(AuthContext);
+
+  const productList = [
     {
-        id:"album1",
-        title: "Colors",
-        price: 100,
-        imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-      },
-      {
-        id:"album2",
-        title: "Black and white Colors",
-        price: 50,
-        imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-      },
-      {
-        id:"album3",
-        title: "Yellow and Black Colors",
-        price: 70,
-        imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-      },
-      {
-        id:"album4",
-        title: "Blue Color",
-        price: 100,
-        imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-      },
-    ];
-  
+      id: "album1",
+      title: "Colors",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+    {
+      id: "album2",
+      title: "Black and white Colors",
+      price: 50,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+    {
+      id: "album3",
+      title: "Yellow and Black Colors",
+      price: 70,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+    {
+      id: "album4",
+      title: "Blue Color",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
+
   const [cart, setCart] = useState([]);
 
-  const addItem = async (id)=>{
-    try{
-      const addedProduct = productList.find((product)=> product.id===id);
-      const addedProductIndex= cart.find((product)=> product.id===id);
-      if(addedProductIndex){
-        alert("The item is already in the cart!!")
+  const addItem = async (id) => {
+    try {
+      const addedProduct = productList.find((product) => product.id === id);
+      const addedProductIndex = cart.find((product) => product.id === id);
+      if (addedProductIndex) {
+        alert("The item is already in the cart!!");
       }
-      if(addedProduct && !addedProductIndex){  
-        const userMail= localStorage.getItem('email');
-        const response= await axios.post(`https://crudcrud.com/api/0da8b18f966f4518ba06563b1ce9ee2e/cart${userMail}`, addedProduct)
-        const addedProductWithId={...addedProduct, _id:response.data._id}
-        setCart((prevCart)=>[...prevCart, addedProductWithId]);
+      if (addedProduct && !addedProductIndex) {
+        const userMail = localStorage.getItem("email");
+        const response = await axios.post(
+          `https://crudcrud.com/api/0da8b18f966f4518ba06563b1ce9ee2e/cart${userMail}`,
+          addedProduct,
+        );
+        const addedProductWithId = { ...addedProduct, _id: response.data._id };
+        setCart((prevCart) => [...prevCart, addedProductWithId]);
       }
-    } catch(err){
+    } catch (err) {
       console.log("Product not added- " + err);
     }
-  } 
+  };
 
-  const removeItem = async (itemid, id)=>{
-    try{
-      setCart((prevCart)=> prevCart.filter((item)=>item.id!==itemid));
-      const userMail= localStorage.getItem('email');
-      await axios.delete(`https://crudcrud.com/api/0da8b18f966f4518ba06563b1ce9ee2e/cart${userMail}/${id}`)
-   } catch(err){
+  const removeItem = async (itemid, id) => {
+    try {
+      setCart((prevCart) => prevCart.filter((item) => item.id !== itemid));
+      const userMail = localStorage.getItem("email");
+      await axios.delete(
+        `https://crudcrud.com/api/0da8b18f966f4518ba06563b1ce9ee2e/cart${userMail}/${id}`,
+      );
+    } catch (err) {
       console.log("Item not removed- " + err);
-    }     
-        }
-        const fetchItems= async()=>{
-          try{
-            const userMail= localStorage.getItem('email');
-           const response= await axios.get(`https://crudcrud.com/api/0da8b18f966f4518ba06563b1ce9ee2e/cart${userMail}`);
-          setCart(response.data);
-          }catch(err){
-            console.log("Fetch Items failed- " + err);
-          }
-        }
-        
-        useEffect(()=>{ 
-          if(authCtx.isLoggedIn){
-            fetchItems();
-          }       
-        },[])
-       
+    }
+  };
+  const fetchItems = async () => {
+    try {
+      const userMail = localStorage.getItem("email");
+      const response = await axios.get(
+        `https://crudcrud.com/api/0da8b18f966f4518ba06563b1ce9ee2e/cart${userMail}`,
+      );
+      setCart(response.data);
+    } catch (err) {
+      console.log("Fetch Items failed- " + err);
+    }
+  };
+
+  useEffect(() => {
+    if (authCtx.isLoggedIn) {
+      fetchItems();
+    }
+  }, [authCtx.isLoggedIn]);
 
   return (
-    <MyContext.Provider value={{productList, cart, addItem, removeItem}}>{props.children}</MyContext.Provider>
-  )
-}
+    <MyContext.Provider value={{ productList, cart, addItem, removeItem }}>
+      {props.children}
+    </MyContext.Provider>
+  );
+};
 
-export {AppContext, MyContext}
+export { AppContext, MyContext };
